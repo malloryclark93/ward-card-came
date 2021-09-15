14f7967eda8630b0e63b2160c15f4e42311f0149
malloryclark93 <mallory_clark@live.com>
2021-09-15 15:21:55 -0600

    data displayed on button click

---
+++ js/app.js
@@ -0,0 +1,7 @@
+
+document.getElementById('new-deck').addEventListener('click', function() {
+  fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
+  .then(response => response.json())
+.then(data => console.log(data))
+
+})

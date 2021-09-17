30e279a5582b508f6adbef344fa2a156c6dbe23a
malloryclark93 <mallory_clark@live.com>
2021-09-17 12:57:42 -0600

    data from 2 new cards

--- js/app.js
+++ js/app.js
@@ -1,7 +1,27 @@
+let deckId = ''
 
+// data displayed on button click
 document.getElementById('new-deck').addEventListener('click', function() {
-  fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
+  fetch('https://deckofcardsapi.com/api/deck/new/shuffle/')
   .then(response => response.json())
-.then(data => console.log(data))
-
+  .then(data => {
+  deckId = data.deck_id
+  console.log(data)
+    })
 })
+
+document.getElementById('draw-cards').addEventListener('click', function(){
+  console.log(deckId)
+  fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+  })
+})
+
+// document.getElementById("draw-cards").addEventListener("click", () => {
+//   fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+//       .then(res => res.json())
+//       .then(data => console.log(data))
+// })
+
